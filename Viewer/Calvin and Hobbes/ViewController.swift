91c62bd104db51d290393d05302f83d6d64e944f
Adam Van Prooyen <adam.vanpr@gmail.com>
2015-09-30 20:25:01 -0400

    Added Xcode project/workspace for viewer.

---
+++ Viewer/Calvin and Hobbes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calvin and Hobbes
+//
+//  Created by Adam Van Prooyen on 9/27/15.
+//  Copyright © 2015 Adam Van Prooyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Viewer/Calvin and HobbesUITests/Calvin_and_HobbesUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Calvin_and_HobbesUITests.swift
+//  Calvin and HobbesUITests
+//
+//  Created by Adam Van Prooyen on 9/27/15.
+//  Copyright © 2015 Adam Van Prooyen. All rights reserved.
+//
+
+import XCTest
+
+class Calvin_and_HobbesUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
